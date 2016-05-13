a99ae69e8b835f7947370c8d71c2835d688fa5e6
Jackwong123 <1498671522@qq.com>
2016-05-13 17:02:39 +0800

    aaa

---
+++ app/src/main/java/com/example/day30_draw_9_patch/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.day30_draw_9_patch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
